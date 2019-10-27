49bbb09ae1c21a21a2dc5c395864707a398dbefd
MNihal17 <56453829+MNihal17@users.noreply.github.com>
2019-10-27 17:01:22 -0400

    Override the move methods in Barrier.py
    This commit adds the move methods in the Barrier class. The move methods in this commit override the move methods in the Actor class, since the Barriers cannot move. Thus, this change is important for the Barrier class to function properly.

--- Barrier.py
+++ Barrier.py
@@ -6,3 +6,15 @@ class Barrier(Actor):
     def __init__(self, start_xpos: float, start_ypos: float, width: float, height: float, sprite):
         super().__init__(start_xpos, start_ypos, width, height, 0, sprite)
        
+    # Overrides the Actor's move methods below, since the Barriers cannot move.
+    def move_left(self) -> None:
+        return None
+    
+    def move_right(self) -> None:
+        return None
+    
+    def move_up(self) -> None:
+        return None
+    
+    def move_down(self) -> None:
+        return None
